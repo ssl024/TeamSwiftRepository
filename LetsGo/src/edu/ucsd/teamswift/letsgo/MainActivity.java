3a6bcd13a5b6123bd898b1bdfd77499f66d0d06c
Eric <eherman1@gmail.com>
2015-04-25 21:31:01 -0700

    eric push test

--- LetsGo/src/edu/ucsd/teamswift/letsgo/MainActivity.java
+++ LetsGo/src/edu/ucsd/teamswift/letsgo/MainActivity.java
@@ -2,7 +2,7 @@
 //Eric push test
 package edu.ucsd.teamswift.letsgo;
 
-import android.app.Activity;
+import android.app.Activity;	
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
