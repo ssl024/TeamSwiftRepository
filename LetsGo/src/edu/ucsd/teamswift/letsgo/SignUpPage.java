4e38761c487c4bbbf3e01b4a31bf44654b72f597
SiWafer <sws559@yahoo.com>
2015-04-30 15:52:48 -0700

    test
    test

--- LetsGo/src/edu/ucsd/teamswift/letsgo/SignUpPage.java
+++ LetsGo/src/edu/ucsd/teamswift/letsgo/SignUpPage.java
@@ -76,7 +76,7 @@ public class SignUpPage extends Activity {
 				else if (!emailString.equals(reEmailString))
 				{
 					/* TODO */
-					Log.e("FieldForms", "EMAIL DOESNT MATCH");
+					Log.e("FieldForms", "You are dumb");
 					/* Tell user that emails are not the same */
 					/* Pop up menu */
 				}
