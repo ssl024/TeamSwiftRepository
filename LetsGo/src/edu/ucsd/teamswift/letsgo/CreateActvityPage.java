9d2b6426a3d19aa88df74dedc6682aee9cd417fc
jakevo <javo@ucsd.edu>
2015-05-07 20:47:57 -0700

    cancel button is working on createActivityPage.java

--- LetsGo/src/edu/ucsd/teamswift/letsgo/CreateActvityPage.java
+++ LetsGo/src/edu/ucsd/teamswift/letsgo/CreateActvityPage.java
@@ -1,16 +1,62 @@
 package edu.ucsd.teamswift.letsgo;
 
 import android.app.Activity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateActvityPage extends Activity {
 
+	private EditText inputStartDate,
+					 inputRepeatDates,
+					 inputStartTime,
+					 inputEndTime,
+					 inputLocation,
+					 inputNumPeople,
+					 inputOtherInfo;
+	private Button cancelCreateBut,
+				   createBut;
+					 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_actvity_page);
+		inputStartDate = (EditText)findViewById(R.id.inputStartDate);
+		inputRepeatDates = (EditText)findViewById(R.id.inputRepeatDates);
+		inputStartTime = (EditText)findViewById(R.id.inputStartTime);
+		inputEndTime = (EditText)findViewById(R.id.inputEndTime);
+		inputLocation = (EditText)findViewById(R.id.inputLocation);
+		inputNumPeople = (EditText)findViewById(R.id.inputNumPeople);
+		inputOtherInfo = (EditText)findViewById(R.id.inputOtherInfo);
+		cancelCreateBut = (Button)findViewById(R.id.cancelCreateBut);
+		createBut = (Button)findViewById(R.id.createBut);
+		
+		
+		//user click on cancel button, go back to homepage
+		cancelCreateBut.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick (View v) {
+				
+				Intent moveToHomePage = new Intent(CreateActvityPage.this, HomePage.class);
+				
+				//Clears all other activities including this one when returning to the Main Activity
+				moveToHomePage.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				
+				startActivity(moveToHomePage);
+				finish();
+				
+			}
+			
+			
+		});
+		
+		
 	}
 	//Jake make fields and pop up for confirmation
 	@Override
